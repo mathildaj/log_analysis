b7844ce69eb4e27a247c5957c2bdd79ca4ed95c4
Ying Xie <yingnew2013@gmail.com>
2017-11-22 18:55:04 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Project Description
+
+This project uses python to generate a report based on analyzing the user logs. The logs are stored in a PostgreSQL database.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+1. Python 3 installed
+2. VirtualBox and Vagrant installed (the version of these two software depends on your machine. I installed VirtualBox 5.1.30 and Vagrant 1.9.5)
+3. Git bash or other command line tool installed
+
+### Installing the application
+
+1. Download or clone this VM onto your local machine: [VM for Log Analysis Project](http://github.com/udacity/fullstack-nanodegree-vm)
+2. Download the database used for this project: [News](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip), unzip it, and place newsdata.sql into vagrant directory, which is shared with your virtual machine.
+3. Download this project repository onto your local machine, and place report.py into the same directory where your newsdata.sql is.
+
+## How to run the application
+
+1. Start your Git Bash command toool.
+2. cd into vagrant folder on your local machine, where you placed your VM image. Such as cd C:\MY_VM\vagrant
+3. At command prompt, type vagrant up ($ vagrant up). This may take a long time to run for the first time. Be patient.
+4. Once Git Bash is at the command prompt again, type vagrant ssh ($ vagrant ssh). This will let your log into your VM.
+5. At the command prompt, type cd /vagrant to go to the relevant directory.
+6. At the command prompt, type psql -d news -f newsdata.sql ($ psql -d news -f newsdata.sql) to load the database (You only need to load the data for the first time).
+7. After step 6 is done, type \q to exit psql, and go back to the VM command prompt.
+8. At the command prompt, type python report.py ($ python report.py). You will see the log analysis reports. The results should match report_output.txt file in this repository.
+
+
+## Authors
+
+* **Ying Xie**
+
+## License
+
+None
+
+## Acknowledgments
+
+* The VM image and database are downloaded from Udacity Full Stack Developer Program.
+
---
+++ report.py
@@ -0,0 +1,106 @@
+import psycopg2
+
+# database name
+DB_NAME = "news"
+
+
+# function to run the query, and fetch the results
+def runQueryFromDB(db_name, query):
+
+    try:
+        # connect to the database
+        db = psycopg2.connect(database=db_name)
+        # cursor to run the query
+        cur = db.cursor()
+        cur.execute(query)
+        # fetch results
+        results = cur.fetchall()
+        # return query results
+        return results
+    except:
+        print("Unable to run the query!")
+    finally:
+        if db:
+            # close db
+            db.close()
+
+
+# function to generate the report for the most popular articles
+def mostPopularArticles():
+
+    # query for report 1: what are the most popular three articles?
+    query = ("select articles.title, count(log.id) as TimesViewed "
+             "from log, articles "
+             "where substring(log.path, 10) = articles.slug "
+             "group by articles.title "
+             "order by TimesViewed desc "
+             "limit 3;")
+
+    results = runQueryFromDB(DB_NAME, query)
+
+    if(results is not None):
+        print("The top three viewed articles are: \n")
+        for result in results:
+            print("\"" + result[0] + "\"" + " -- " + str(result[1]) + " Views"
+                  )
+
+    print("\n")
+
+
+# function to generate the report for the most popular authors
+def mostPopularAuthors():
+
+    # query for report 2: who are the most popular authors of all time?
+    query = ("select authors.name, count(log.id) as TimesViewed "
+             "from log, authors, articles "
+             "where substring(log.path, 10) = articles.slug "
+             "and authors.id = articles.author "
+             "group by authors.name "
+             "order by TimesViewed desc "
+             "limit 4;")
+
+    results = runQueryFromDB(DB_NAME, query)
+
+    if(results is not None):
+        print("The most popular authors of all times are: \n")
+        for result in results:
+            print(result[0] + " -- " + str(result[1]) + " Views")
+
+    print("\n")
+
+
+# function to generate the error report
+def errorReport():
+
+    # query for report 3: On which days did more than 1% of requests lead to
+    # errors?
+    query = ("select errors.what_day, cast(errors.error_count as decimal) / "
+             "totals.total_count as error_percent "
+             "from (select count(log.id) as error_count, "
+             "log.time::date as what_day "
+             "from log "
+             "where log.status != '200 OK' "
+             "group by log.time::date) errors, "
+             "(select count(log.id) as total_count, "
+             "log.time::date as what_day "
+             "from log "
+             "group by log.time::date) totals "
+             "where errors.what_day = totals.what_day "
+             "and cast(errors.error_count as decimal) / "
+             "totals.total_count > 0.01; ")
+
+    results = runQueryFromDB(DB_NAME, query)
+
+    if(results is not None):
+        print("The days with more than 1% of requests lead to errors: \n")
+        for result in results:
+            print(str(result[0]) + " -- " + "{0:.2f}%".format(result[1] * 100))
+
+    print("\n")
+
+
+# run the queries and generate the reports
+if __name__ == "__main__":
+    mostPopularArticles()
+    mostPopularAuthors()
+    errorReport()
